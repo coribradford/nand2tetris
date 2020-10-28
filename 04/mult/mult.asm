1a4e32917a39c72d13fbf05f6379d1521af91ab4
Cori Bradford <bradfcor@oregonstate.edu>
2020-10-28 10:11:34 -0700

    mult working

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,54 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @R2
+    M=0 // zero out end value
+
+    @R1
+    D=M // grab value from register 1
+    @n
+    M=D //set n variable to register 1 value for comparison
+    @i
+    M=1 //set i for count
+    @sum
+    M=0 //initialize sum
+
+    @R0 // check the first register
+    D = M 
+    @STOP //if zero jump to STOP
+    D;JEQ
+
+    @R1 // check the second register
+    D = M 
+    @STOP //if zero jump to STOP
+    D;JEQ
+
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @STOP
+    D;JGT //if i>n goto STOP
+
+    @sum
+    D=M //grab value from sum
+    @R0 
+    D = D+M //add to R0 value
+    @sum
+    M=D //store sum
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP //increment i and jump to LOOP
+
+(STOP)
+    @sum
+    D=M
+    @R2
+    M=D //puts sum value in R2
+
+(END)
+    @LOOP
+    0;JMP //end
