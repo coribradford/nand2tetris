d8963de5e9b93125a932ea0d4e30215bee32e33f
Cori Bradford <bradfcor@oregonstate.edu>
2020-11-05 18:19:50 -0800

    fill working

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,50 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(TOP)
+    @SCREEN
+    D=A
+    @0
+    M=D     //grab screen address and store in ram0 for easier incrementing
+
+(KEYS)
+    @KBD
+    D=M
+    @BLACK
+    D;JGT   //if d is greater than zero some key is pressed, proceed to black
+    @WHITE
+    D;JEQ   //if d is zero no keys are pressed, proceed to white
+
+(BLACK)
+    @1      //hold fill value in register 1
+    M=-1    //determines fill val based on earlier jump condition
+    @FILL
+    0;JMP   //unconditionally jump to fill
+
+(WHITE)
+    @1      //again, holds fill value in register 1
+    M=0     //determines fill val based on earlier jump condition
+    @FILL
+    0;JMP   //unconditionally jump to fill again
+
+(FILL)
+    @1      //check register 1 which is holding color instructions
+    D=M     //grab instruction and hold in d
+    @0      //grab screen address
+    A=M     //go there
+    M=D     //fill pixel based on instruction
+    @0      //go back to register 0 holding screen address
+    D=M+1   //increment by 1 pixel but in data register
+    @KBD    //go to keyboard
+    D=A-D   //store value for check for end of loop; 
+            //keyboard addresses start at end of screen addresses
+    @0      //back to register holding screen address
+    M=M+1   //increment stored screen address by 1 pixel
+    A=M     //goto that address
+    @FILL   //restart loop if incremented pixel value stored in data register doesnt
+    D;JGT   //exceed screen addresses
+
+    @TOP
+    0;JMP  //always restart loop
+
+
