b9f191d5ed2445a6dd868d3194abf1d8009dfd74
Cori Bradford <bradfcor@oregonstate.edu>
2020-11-05 18:30:01 -0800

    cleaned up comments

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,25 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
     @R2
-    M=0 // zero out end value
+    M=0     // zero out end value
 
     @R1
-    D=M // grab value from register 1
+    D=M     // grab value from register 1
     @n
-    M=D //set n variable to register 1 value for comparison
+    M=D     //set n variable to register 1 value for comparison
     @i
-    M=1 //set i for count
+    M=1     //set i for count
     @sum
-    M=0 //initialize sum
+    M=0     //initialize sum
 
-    @R0 // check the first register
+    @R0     // check the first register
     D = M 
-    @STOP //if zero jump to STOP
+    @STOP   //if zero jump to STOP
     D;JEQ
 
-    @R1 // check the second register
+    @R1     // check the second register
     D = M 
-    @STOP //if zero jump to STOP
+    @STOP   //if zero jump to STOP
     D;JEQ
 
 (LOOP)
@@ -34,26 +34,26 @@
     @n
     D=D-M
     @STOP
-    D;JGT //if i>n goto STOP
+    D;JGT   //if i>n goto STOP
 
     @sum
-    D=M //grab value from sum
+    D=M     //grab value from sum
     @R0 
     D = D+M //add to R0 value
     @sum
-    M=D //store sum
+    M=D     //store sum
 
     @i
     M=M+1
     @LOOP
-    0;JMP //increment i and jump to LOOP
+    0;JMP   //increment i and jump to LOOP
 
 (STOP)
     @sum
     D=M
     @R2
-    M=D //puts sum value in R2
+    M=D     //puts sum value in R2
 
 (END)
     @LOOP
-    0;JMP //end
+    0;JMP   //end
