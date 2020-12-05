057259b3a790f75eb7c3d90e7cb21e806307d3de
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 14:06:19 -0800

    got file i/o at command line working

--- 06/assembler.py
+++ 06/assembler.py
@@ -6,20 +6,28 @@
 
 import re, sys
 
-def assembler(file):
+def assembler():
     command_list = []
 
-    f = sys.argv[1]
-    asmFile = open(f+".asm", "r")
+    root = sys.argv[1]
+    asmFile = open(root + ".asm", "r")
+    hackfile = open(root + ".hack", "w")
 
-    for command in asmFile:
-        newline=re.sub(r'\/+.*\n|\n| *','',command)
-        if newline!='':
-            command_list.append(newline)
-
+    for line in asmFile:
+        hackfile.write("ah"+"\n")
+    
     asmFile.close()
+    hackfile.close()
+
+assembler()
+    # for command in asmFile:
+    #     newline=re.sub(r'\/+.*\n|\n| *','',command)
+    #     if newline!='':
+    #         command_list.append(newline)
+
+    # asmFile.close()
 
 
-    hackFile = open(f+'.hack','w')
-    hackFile.write("AHHH")
-    hackFile.close()
+    # hackFile = open(f+'.hack','w')
+    # hackFile.write("AHHH")
+    # hackFile.close()
