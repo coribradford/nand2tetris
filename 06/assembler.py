774eaa60e6e71fd07f735d0a87620d22baeea8f8
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 15:14:17 -0800

    added handling for wrong file extention

--- 06/assembler.py
+++ 06/assembler.py
@@ -1,21 +1,28 @@
 
 
 import sys
+
+
 inputFileName = sys.argv[1]
+if "." in inputFileName:
+    asm = ".asm"
+    if asm in inputFileName:
+        inputFileName = inputFileName.replace(".asm", "")
+    else:
+        raise ValueError("Please input the correct .asm file type.")
 
 def parser():
     command_list = []
 
     asmFile = open(inputFileName + ".asm", "r")
-    hackfile = open(inputFileName + ".hack", "w")
+    hackFile = open(inputFileName + ".hack", "w")
 
     for line in asmFile:
         cleanedLine = cleanup(line)
         if cleanedLine != "":
-            hackfile.write(cleanedLine + "\n")
+            hackFile.write(cleanedLine + "\n")
     
     asmFile.close()
-    hackfile.close()
 
 
 def cleanup(fileLine):
