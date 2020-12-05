0a9749daf30dbe5ad1424a1b61f7044065deaff6
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 14:31:54 -0800

    working on removing comments and whitespace

--- 06/assembler.py
+++ 06/assembler.py
@@ -2,32 +2,33 @@
 
 
 
-
-
 import re, sys
-
+inputFile = sys.argv[1]
 def assembler():
     command_list = []
 
-    root = sys.argv[1]
-    asmFile = open(root + ".asm", "r")
-    hackfile = open(root + ".hack", "w")
+    
+    asmFile = open(inputFile + ".asm", "r")
+    hackfile = open(inputFile + ".hack", "w")
 
     for line in asmFile:
-        hackfile.write("ah"+"\n")
+        hackfile.write(cleanup(line))
     
     asmFile.close()
     hackfile.close()
 
+
+
+
+
+
+def cleanup(fileLine):
+    firstCharacter = fileLine[0]
+    if firstCharacter == "\n" or firstCharacter == "/":
+        return ""
+    elif firstCharacter == " ":
+        return cleanup(fileLine[1:])
+    else:
+        return firstCharacter + cleanup(fileLine[1:])
+
 assembler()
-    # for command in asmFile:
-    #     newline=re.sub(r'\/+.*\n|\n| *','',command)
-    #     if newline!='':
-    #         command_list.append(newline)
-
-    # asmFile.close()
-
-
-    # hackFile = open(f+'.hack','w')
-    # hackFile.write("AHHH")
-    # hackFile.close()
