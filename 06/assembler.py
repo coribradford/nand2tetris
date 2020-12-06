a9701ae86994e2fd02b0d3a59d839b418726acfb
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 23:12:26 -0800

    not working? debugging

--- 06/assembler.py
+++ 06/assembler.py
@@ -12,17 +12,20 @@ if "." in inputFileName:
         raise ValueError("Please input the correct .asm file type.")
 
 
-symbolTable={"SP":0,"LCL":1,"ARG":2,"THIS":3,"THAT":4,"SCREEN":16384,"KBD":24576,
+symbolTable = {"SP":0,"LCL":1,"ARG":2,"THIS":3,"THAT":4,"SCREEN":16384,"KBD":24576,
     "R0":0,"R1":1,"R1":1,"R2":2,"R3":3,"R4":4,"R5":5,"R6":6,"R7":7,
     "R8":8,"R9":9,"R10":10,"R11":11,"R12":12,"R13":13,"R14":14,"R15":15}
 
-destTable={" ":"000","M":"001","D":"010","MD":"011",
+
+destTable = {" ":"000","M":"001","D":"010","MD":"011",
     "A":"100","AM":"101","AD":"110","AMD":"111"}
     
-jumpTable={" ":"000","JGT":"001","JEQ":"010","JGE":"011",
+
+jumpTable = {" ":"000","JGT":"001","JEQ":"010","JGE":"011",
     "JLT":"100","JNE":"101","JLE":"110","JMP":"111"}
     
-compTable={"0":"0101010","1":"0111111","-1":"0111010","D":"0001100",
+
+compTable = {"0":"0101010","1":"0111111","-1":"0111010","D":"0001100",
     "A":"0110000","M":"1110000","!D":"0001101","!A":"0110001",
     "!M":"1110001","-D":"0001111","-A":"0110011","-M":"1110011",
     "D+1":"0011111","A+1":"0110111","M+1":"1110111","D-1":"0001110",
@@ -38,12 +41,11 @@ variablenumber = 16
 def parser():
     asmFile = open(inputFileName + ".asm", "r")
     tempFile = open(inputFileName + ".tmp", "w")
-
     for line in asmFile:
         cleanedLine = cleanup(line)
-        if cleanedLine != "":
-            tempFile.write(cleanedLine + "\n")
-    
+        firstpassline = firstPass(cleanedLine)
+        if firstpassline != "":
+            tempFile.write(firstpassline + "\n")
     asmFile.close()
     tempFile.close()
 
@@ -63,6 +65,7 @@ def firstPass(line):
     if line[0] == "(":
         label = line[1:-1]
         symbolTable[label] = linenumber 
+        return ""
     else:
         linenumber += 1
         return line
@@ -74,11 +77,12 @@ def secondPass(line):
     else:
         return c_instruction(line)
 
+
 def a_instruction(line):
     global variablenumber
     if line[1].isalpha():
         label = line[1:-1]
-        value = symbols.get(label, -1)
+        value = symbolTable.get(label, -1)
         if value == -1:
             value = a_instruction_prep(label)
     else:
@@ -89,9 +93,9 @@ def a_instruction(line):
 
 def a_instruction_prep(label):
     global variablenumber
-    symbols[label] = variablenumber
+    symbolTable[label] = variablenumber
     variablenumber += 1
-    return symbols[label]
+    return symbolTable[label]
 
 
 def c_instruction(line):
@@ -118,8 +122,7 @@ def performPasses():
     tempFile = open(inputFileName + ".tmp", "r")
     hackFile = open(inputFileName + ".hack", "w")
     for line in tempFile:
-        firstpassLine = firstPass(line)
-        secondpassLine = secondPass(firstpassLine)
+        secondpassLine = secondPass(line)
         hackFile.write(secondpassLine + "\n")
     tempFile.close()
     os.remove(inputFileName + ".tmp")
@@ -128,4 +131,3 @@ def performPasses():
 
 parser()
 performPasses()
-
