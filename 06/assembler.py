f02585105272f691d6062937b826e943f137f3a7
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 22:11:52 -0800

    a instruction working, c mostly

--- 06/assembler.py
+++ 06/assembler.py
@@ -1,6 +1,6 @@
 
 
-import sys
+import sys, os
 
 
 inputFileName = sys.argv[1]
@@ -12,9 +12,23 @@ if "." in inputFileName:
         raise ValueError("Please input the correct .asm file type.")
 
 
-symbols={'SP':0,'LCL':1,'ARG':2,'THIS':3,'THAT':4,'SCREEN':16384,'KBD':24576,
-         'R0':0,'R1':1,'R1':1,'R2':2,'R3':3,'R4':4,'R5':5,'R6':6,'R7':7,
-         'R8':8,'R9':9,'R10':10,'R11':11,'R12':12,'R13':13,'R14':14,'R15':15}
+symbolTable={'SP':0,'LCL':1,'ARG':2,'THIS':3,'THAT':4,'SCREEN':16384,'KBD':24576,
+    'R0':0,'R1':1,'R1':1,'R2':2,'R3':3,'R4':4,'R5':5,'R6':6,'R7':7,
+    'R8':8,'R9':9,'R10':10,'R11':11,'R12':12,'R13':13,'R14':14,'R15':15}
+
+destTable={'':'000','M':'001','D':'010','MD':'011',
+    'A':'100','AM':'101','AD':'110','AMD':'111'}
+    
+jumpTable={'':'000','JGT':'001','JEQ':'010','JGE':'011',
+    'JLT':'100','JNE':'101','JLE':'110','JMP':'111'}
+    
+compTable={'0':'0101010','1':'0111111','-1':'0111010','D':'0001100',
+    'A':'0110000','M':'1110000','!D':'0001101','!A':'0110001',
+    '!M':'1110001','-D':'0001111','-A':'0110011','-M':'1110011',
+    'D+1':'0011111','A+1':'0110111','M+1':'1110111','D-1':'0001110',
+    'A-1':'0110010','M-1':'1110010','D+A':'0000010','D+M':'1000010',
+    'D-A':'0010011','D-M':'1010011','A-D':'0000111','M-D':'1000111',
+    'D&A':'00000000','D&M':'1000000','D|A':'0010101','D|M':'1010101'}
 
 
 linenumber = 0
@@ -48,7 +62,7 @@ def firstPass(line):
     global linenumber
     if line[0] == "(":
         label = line[1:-1]
-        symbols[label] = linenumber 
+        symbolTable[label] = linenumber 
     else:
         linenumber += 1
         return line
@@ -82,12 +96,14 @@ def a_instruction_prep(label):
 
 def c_instruction(line):
     line = c_instruction_prep(line)
-    print(line)
     destSplit = line.split("=")
-    #perform grab from table for dest; build table
-    jumpSplit = destSplit.split(";")
-    #perform grab from table for comp and jump
-    #return stitched new line
+    dest = destTable.get(destSplit[0])
+    jumpSplit = destSplit[1].split(";")
+    comp = compTable.get(jumpSplit[0])
+    jump = jumpTable.get(jumpSplit[1])
+    c_inst = "111" + str(dest) + str(comp) + str(jump)
+    return c_inst
+
 
 def c_instruction_prep(line):
     if "=" not in line:
@@ -99,15 +115,16 @@ def c_instruction_prep(line):
 
 def performPasses():
     tempFile = open(inputFileName + ".tmp", "r")
-    temp2File = open(inputFileName + "1.tmp", "w")
+    hackFile = open(inputFileName + ".hack", "w")
     for line in tempFile:
         firstpassLine = firstPass(line)
         secondpassLine = secondPass(firstpassLine)
-        temp2File.write(secondpassLine)
+        hackFile.write(secondpassLine + "\n")
     tempFile.close()
-    temp2File.close()
-
+    os.remove(inputFileName + ".tmp")
+    hackFile.close()
 
 
 parser()
 performPasses()
+
