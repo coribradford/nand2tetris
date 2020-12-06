2b4032e8f364ac75cd94d671881506a43e2f0abb
Cori Bradford <bradfcor@oregonstate.edu>
2020-12-05 21:58:18 -0800

    a mostly working, progress on c

--- 06/assembler.py
+++ 06/assembler.py
@@ -16,11 +16,12 @@ symbols={'SP':0,'LCL':1,'ARG':2,'THIS':3,'THAT':4,'SCREEN':16384,'KBD':24576,
          'R0':0,'R1':1,'R1':1,'R2':2,'R3':3,'R4':4,'R5':5,'R6':6,'R7':7,
          'R8':8,'R9':9,'R10':10,'R11':11,'R12':12,'R13':13,'R14':14,'R15':15}
 
+
 linenumber = 0
 variablenumber = 16
 
-def parser():
 
+def parser():
     asmFile = open(inputFileName + ".asm", "r")
     tempFile = open(inputFileName + ".tmp", "w")
 
@@ -54,11 +55,11 @@ def firstPass(line):
 
 
 def secondPass(line):
-    print(line)
     if line[0] == "@":
         return a_instruction(line)
     else:
         return c_instruction(line)
+
 
 def a_instruction(line):
     global variablenumber
@@ -72,7 +73,6 @@ def a_instruction(line):
     newvalue = bin(value)[2:].zfill(16)
     return newvalue
 
-    
 
 def a_instruction_prep(label):
     global variablenumber
@@ -82,7 +82,12 @@ def a_instruction_prep(label):
 
 def c_instruction(line):
     line = c_instruction_prep(line)
-    return line
+    print(line)
+    destSplit = line.split("=")
+    #perform grab from table for dest; build table
+    jumpSplit = destSplit.split(";")
+    #perform grab from table for comp and jump
+    #return stitched new line
 
 def c_instruction_prep(line):
     if "=" not in line:
